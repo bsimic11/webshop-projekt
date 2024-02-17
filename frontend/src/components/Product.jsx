ebd5f3b6fd4131b4c130096707cb2c322ae4a70a
bsimic11 <bartul.simic@gmail.com>
2024-02-17 15:04:23 +0100

    implementiran backend za login,logout,register

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -20,7 +20,7 @@ const Product= ({product}) => {
                 <Rating value={product.rating} text={ `${product.numReviews} reviews` } />
             </Card.Text>
             <Card.Text as="h3">
-                ${product.price}
+                €{product.price}
             </Card.Text>
         </Card.Body>
 
